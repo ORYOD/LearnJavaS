493ed39bb85c937d617e89a055b5dbe8b22dda46
dmarci <danielemg1997@hotmail.com>
2022-06-02 11:39:44 -0400

    git rules

---
+++ git.js
@@ -0,0 +1,13 @@
+/* 
+repaso
+
+git add (+): añade los archivos seleccionados al  area de stage
+
+git commit: hace una versión con los archivos modificados identificado con un nombre
+
+git push: sincroniza los cambios al repositorio remoto
+
+git pull: se trae los cambios de las otras personas
+
+
+*/
