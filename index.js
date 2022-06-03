c10a42aa520302740ab6bef25e01055a92c12864
dmarci <danielemg1997@hotmail.com>
2022-06-03 12:13:01 -0400

    objetos

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//hola+chao+hello!!!!
+//* tipos de datos
+//*metodos para los tipos de datos 
+
+//APIS, BACKEND responden en objetos o array de objetos
+
+//objeto
+/*
+
+paso 1: se escribe el tipo de variable: const o let
+paso 2: se define el nombre de la variable
+paso 3: se iguala la variable a la sintanxis de objeto
+
+sintaxis de objeto:   {key: value, key2:value2,...... keyn: valuen }
+para añadir elementos al objeto: objeto.nuevaKey = nuevo valor a insertar
+
+para mostrar un valor de un objeto: objeto.key
+
+para logear: console.log(valor a loggeear)
+
+*/
+
+
+//1 crear un objeto
+//2 añadir un nuevo key 
+//3 loggear el objeto
+//4 loggear un key del objeto
+
